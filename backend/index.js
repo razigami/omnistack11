892ffddaa6898867071ca55f0a1973c985031ce3
Eduardo Martins <eduardo.ribeiro57@etec.sp.gov.br>
2020-03-29 13:20:00 -0300

    first commit

---
+++ backend/index.js
@@ -0,0 +1,6 @@
+const express = require ('express'); //importar o modulo express para a variavel `express`
+const app = express(); //instanciar
+app.get('/', (request, response) =>{
+
+});
+app.listen (3333);
